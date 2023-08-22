cfdc939f4c97cf550389c2eb00d6b6937bceb711
phillipm-developer <14167@coderacademy.edu.au>
2023-08-22 15:37:31 +1000

    Initial commit - Backend Setup

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import { UserModel, BookModel, UserInventoryModel, MessageModel, dbClose } from "./db.js"
+import cors from 'cors'
+
+const app = express()
+
+// Allows anyone to connect by default (good for development but not production)
+// in production you add specific origins
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/', (request, response) => response.send({ info: 'BookSwapU API!' }))
+
+app.get('/users', async (req, res) => res.send(await UserModel.find()))
+
+app.get('/books', async (req, res) => res.send(await BookModel.find()))
+
+app.get('/user_inventory', async (req, res) => res.send(await UserInventoryModel.find()))
+
+app.get('/messages', async (req, res) => res.send(await MessageModel.find()))
+
+export default app

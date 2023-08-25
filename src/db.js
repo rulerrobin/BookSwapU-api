074ad2135906b6da79c09eccd4155ab4dc202043
Genuel Mark <gmzapanta98@gmail.com>
2023-08-25 17:47:33 +1000

    removed pic from userSchema as not working for now, also moved error middleware due to errors being cause from misposition

--- src/db.js
+++ src/db.js
@@ -23,11 +23,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  pic: {
-    type: String,
-    default:
-      'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg'
-    },
   },
   { 
     timesamps: true 

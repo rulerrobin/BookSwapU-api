c22d6d9fd5ea6b5c1b4d8ad65f08775600e81cc5
phillipm-developer <14167@coderacademy.edu.au>
2023-08-25 14:39:54 +1000

    Added PUT /users/:user_id & GET /books/:book_id routes

--- src/app.js
+++ src/app.js
@@ -24,6 +24,145 @@ app.get('/', (request, response) => response.send({ info: 'BookSwapU API!' }))
 // GET method request handler for retrieving all users data.
 app.get('/users', async (req, res) => res.send(await UserModel.find()))
 
+// Gets routes from userRoutes.js
+app.use('/api/user', userRoutes)
+
+// PUT method request handler for updating the current user's details (Profile).
+// Note: When there is a password change, the property 'old_password' has to be 
+// provided in the json request body and it has to match the password on record for the
+// update to task place.
+app.put('/users/:user_id', async (req,  res) => {
+    try {
+        // Get the current user
+        const currentUser = await UserModel.findById(req.params.user_id)
+
+        // Create a separate user object for update
+        let userDetails = { username: req.body.username, email: req.body.email }
+
+        // If password property exists in request body
+        if (req.body.password) {
+            // If password in request body is the same as password on record
+            if (req.body.password === currentUser.password) {
+                userDetails.password = req.body.password
+            }
+            else {  // If 'old_password' property is the same as password on record
+                if (req.body.old_password === currentUser.password) {
+                    userDetails.password = req.body.password
+                }
+                else {
+                    res.status(404).send({ error: 'Old password is not on record'})
+                    return
+                }
+            }
+        }
+
+        // Update the user with new details
+        const updatedUser = await UserModel.findByIdAndUpdate(req.params.user_id, userDetails, { new: true })
+
+        if (updatedUser) {
+            res.status(201).send(updatedUser)
+        }
+        else {
+            res.status(404).send({ error: 'User not found' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+// PUT method request handler for updating the current user's details (Profile).
+// Note: When there is a password change, the property 'old_password' has to be 
+// provided in the json request body and it has to match the password on record for the
+// update to task place.
+app.put('/users/:user_id', async (req,  res) => {
+    try {
+        // Get the current user
+        const currentUser = await UserModel.findById(req.params.user_id)
+
+        // Create a separate user object for update
+        let userDetails = { username: req.body.username, email: req.body.email }
+
+        // If password property exists in request body
+        if (req.body.password) {
+            // If password in request body is the same as password on record
+            if (req.body.password === currentUser.password) {
+                userDetails.password = req.body.password
+            }
+            else {  // If 'old_password' property is the same as password on record
+                if (req.body.old_password === currentUser.password) {
+                    userDetails.password = req.body.password
+                }
+                else {
+                    res.status(404).send({ error: 'Old password is not on record'})
+                    return
+                }
+            }
+        }
+
+        // Update the user with new details
+        const updatedUser = await UserModel.findByIdAndUpdate(req.params.user_id, userDetails, { new: true })
+
+        if (updatedUser) {
+            res.status(201).send(updatedUser)
+        }
+        else {
+            res.status(404).send({ error: 'User not found' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+// PUT method request handler for updating the current user's details (Profile).
+// Note: When there is a password change, the property 'old_password' has to be 
+// provided in the json request body and it has to match the password on record for the
+// update to task place.
+app.put('/users/:user_id', async (req,  res) => {
+    try {
+        // Get the current user
+        const currentUser = await UserModel.findById(req.params.user_id)
+
+        // Create a separate user object for update
+        let userDetails = { username: req.body.username, email: req.body.email }
+
+        // If password property exists in request body
+        if (req.body.password) {
+            // If password in request body is the same as password on record
+            if (req.body.password === currentUser.password) {
+                userDetails.password = req.body.password
+            }
+            else {  // If 'old_password' property is the same as password on record
+                if (req.body.old_password === currentUser.password) {
+                    userDetails.password = req.body.password
+                }
+                else {
+                    res.status(404).send({ error: 'Old password is not on record'})
+                    return
+                }
+            }
+        }
+
+        // Update the user with new details
+        const updatedUser = await UserModel.findByIdAndUpdate(req.params.user_id, userDetails, { new: true })
+
+        if (updatedUser) {
+            res.status(201).send(updatedUser)
+        }
+        else {
+            res.status(404).send({ error: 'User not found' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+// User error handlers
+app.use(notFound)
+app.use(errorHandler)
+
 // GET method request handler which returns the books associated with 
 // a given user.
 app.get('/users/:user_id/books', async (req, res) => {
@@ -80,6 +219,23 @@ app.delete('/users/:user_id/books/:book_id',  async (req, res) => {
 
 // GET method request handler which returns the entire 'book' collection
 app.get('/books', async (req, res) => res.send(await BookModel.find()))
+
+// GET method request handler which returns individual books based on 'book_id' attribute
+app.get('/books/:book_id', async (req,  res) => {
+    try {
+        const book = await BookModel.findById(req.params.book_id)
+
+        if (book) {
+            res.status(201).send(book)
+        }
+        else {
+            res.status(404).send({ error: 'Book not found' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
 
 // POST method request handler allows insertion of books into the 'books' collection.
 // Note: The books are NOT associated with a user through 'UserInventoryModel'.

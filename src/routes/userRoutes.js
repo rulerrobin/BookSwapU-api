6ad5c65e6ad5dffe08f6ef0ae8d303b86a20952e
Genuel Mark <gmzapanta98@gmail.com>
2023-08-28 14:48:33 +1000

    changed how router was accessed in userRoutes to be more consistent

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,19 @@
-// Import the 'express' module for creating a router
-import express from "express"
+// Import Router from express
+import { Router } from 'express'
 // Import the 'registerUser' and 'authUser' functions from 'userControllers.js'
 import { registerUser, authUser }  from '../controllers/userControllers.js'
+
 // Create an instance of an Express Router
-const router = express.Router()
+const router = Router()
+
 // Define routes and their corresponding HTTP methods
 // 'router.route()' creates a chainable route handler for the specified path
 
 // Handle POST requests to the root path ('/') by calling the 'registerUser' function
-router.route('/').post(registerUser)
+router.post('/', registerUser)
+
 // Handle POST requests to the '/login' path by calling the 'authUser' function
-router.route('/login').post(authUser)
+router.post('/login', authUser)
 
 // Export the router instance to be used in other parts of the application
 export default router 

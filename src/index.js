48781086170a91381688bdc3d84c20a97862dde9
Genuel Mark <gmzapanta98@gmail.com>
2023-09-02 01:22:26 +1000

    added socket.io realtime messaging, changed app listen to variable so server is able to use it

--- src/index.js
+++ src/index.js
@@ -2,9 +2,45 @@
 import app from './app.js'
 // Import the 'dotenv' module to load environment variables from a '.env' file
 import dotenv from 'dotenv'
+
+import { Server } from "socket.io"
 // Load environment variables from the '.env' file
 dotenv.config()
 // Define the port number on which the server will listen with 5000 as backup
 const PORT = process.env.PORT || 5000
 // Start the server and listen on the specified port
-app.listen(PORT, console.log(`Server Started on ${PORT}`)) // Output a message to the console indicating that the server has started
+const server = app.listen(PORT, console.log(`Server Started on ${PORT}`)) // Output a message to the console indicating that the server has started
+// Socket.io
+const io = new Server(server, {
+   pingTimeout: 60000,
+   cors: {
+      origin: "http://localhost:3001", // the local host of frontend when you do npm run dev
+   }
+})
+
+io.on("connection", (socket) => {
+   console.log('Connected to socket.io')
+
+   socket.on("setup", (userData) => {
+      socket.join(userData._id)
+      // console.log(userData._id)
+      socket.emit("connected")
+   })
+
+   socket.on("join chat", (room) => {
+      socket.join(room)
+      console.log("User Joined Room: " + room)
+   })
+
+   socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat
+
+      if (!chat.users) return console.log("chat.users not defined")
+
+      chat.users.forEach((user) => {
+         if (user._id == newMessageRecieved.sender._id) return
+
+         socket.in(user._id).emit("message recieved", newMessageRecieved)
+      })
+   })
+})
